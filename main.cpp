87073479d974c3add499933b12b985b329c4565b
yangb6 <ybgogo@hotmail.com>
2016-05-07 21:20:00 -0400

    virtual memory done

--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@
 #include <assert.h>
 #include "pair.h"
 #include "Memory.h"
+#include <vector>
+#include "virtual_memory.h"
+#include <list>
 using namespace std;
 
 # define t_memmove 1 /* time of moving each unit in defragmentation  */
@@ -184,6 +187,288 @@ void start_simulate(my_map event_map, Memory memory, const string& alg) {
     if(alg != "Non-contiguous") cout << "Contiguous -- ";
     cout << alg << ")\n\n" << endl;
 }
+//================================================Virtual Memory==================================
+void read_file_2(ifstream &in_str, vector<int> &page_reference, map<int,list<int> > &page_index)
+{
+    int count = 0;
+    int tmp;
+    while(in_str>>tmp)
+    {
+        page_reference.push_back(tmp);
+        page_index[tmp].push_back(count);
+        count++;
+    }
+}
+
+void print_frame( int frame[])
+{
+    cout<<"[mem: ";
+    for(int i = 0 ; i < F;i++)
+    {
+        if(frame[i] >= 0 && i != (F-1))
+        {
+            cout<<frame[i]<<" ";
+        }
+        else if(frame[i] >= 0)
+        {
+            cout<<frame[i];
+        }
+        else if(i == (F-1))
+        {
+            cout<<".";
+        }
+        else
+        {
+            cout<<". ";
+        }
+    }
+    cout<<"]";
+}
+
+//------------------------------------------------OPT replacement algo----------------------------
+void OPT(vector<int> page_reference, map<int,list<int> > page_index, Virtual_memory virtual_memory)
+{
+    int count_fault = 0;
+    cout<<"Simulating OPT with fixed frame size of "<<F<<endl;
+    for(int i = 0; i < page_reference.size();i++)
+    {
+        bool is_find = false;
+        for(int l = 0 ; l < F;l++)
+        {
+            if(page_reference[i] == virtual_memory.frame[l])
+            {
+                is_find = true;
+                break;
+            }
+        }
+        if( !is_find)
+        {
+            if(virtual_memory.get_left_unused() > 0)
+            {
+                    int j = 0;
+                    while(virtual_memory.frame[j] >= 0)
+                    {
+                        j++;
+                    }
+                    virtual_memory.frame[j] = page_reference[i];
+                    page_index[page_reference[i]].pop_front();
+                    int tmp = virtual_memory.get_left_unused();
+                    virtual_memory.set_left_unused(--tmp);
+                    cout<<"referencing page "<<page_reference[i]<<" ";
+                    print_frame(virtual_memory.frame);
+                    cout<<" PAGE FAULT (no victim page)"<<endl;
+                    count_fault++;
+            }
+            else
+            {
+                int replace_page = -1;
+                int replace_page_index = -2;
+                int replace_page_frame_index = -1;
+                for(int m = 0 ; m < F; m++)
+                {
+                    int tmp = -1;
+                    list<int>::iterator itr = page_index[virtual_memory.frame[m]].begin();
+                    if(itr != page_index[virtual_memory.frame[m]].end())
+                    {
+                        tmp = *itr;
+                    }
+                    if(tmp != -1 && replace_page_index != -1)
+                    {
+                        if(tmp > replace_page_index )
+                        {
+                            replace_page = virtual_memory.frame[m];
+                            replace_page_frame_index = m;
+                            replace_page_index = tmp;
+                        }
+                    }
+                    else if(tmp == -1)
+                    {
+                        if(replace_page_index == -1)
+                        {
+                            if(replace_page > virtual_memory.frame[m])
+                            {
+                                replace_page = virtual_memory.frame[m];
+                                replace_page_frame_index = m;
+                                replace_page_index = tmp;
+                            }
+                        }
+                        else
+                        {
+                            replace_page = virtual_memory.frame[m];
+                            replace_page_frame_index = m;
+                            replace_page_index = tmp;
+                        }
+                    }
+                }
+                int victim_page = virtual_memory.frame[replace_page_frame_index];
+                virtual_memory.frame[replace_page_frame_index] = page_reference[i];
+                page_index[page_reference[i]].pop_front();
+                cout<<"referencing page "<<page_reference[i]<<" ";
+                print_frame(virtual_memory.frame);
+                cout<<" PAGE FAULT (victim page "<<victim_page<<")"<<endl;
+                count_fault++;
+
+            }
+        }
+        else
+        {
+            cout<<"referencing page "<<page_reference[i]<<" ";
+            print_frame(virtual_memory.frame);
+            cout<<endl;
+            page_index[page_reference[i]].pop_front();
+        }
+    }
+    cout<<"End of OPT simulation ("<<count_fault<<" page faults)"<<endl;
+    cout<<endl;
+}
+//------------------------------------------------LRU replacement algo----------------------------
+void LRU(vector<int> page_reference, Virtual_memory virtual_memory)
+{
+
+    int count_fault = 0;
+    cout<<"Simulating LRU with fixed frame size of "<<F<<endl;
+    for(int i = 0; i < page_reference.size();i++)
+    {
+        bool is_find = false;
+        for(int l = 0 ; l < F;l++)
+        {
+            if(page_reference[i] == virtual_memory.frame[l])
+            {
+                is_find = true;
+                break;
+            }
+        }
+        if(!is_find)
+        {
+            //Frame is not full of pages
+            if(virtual_memory.get_left_unused() > 0)
+            {
+                int j = 0;
+                while(virtual_memory.frame[j] >= 0)
+                {
+                    j++;
+                }
+                virtual_memory.frame[j] = page_reference[i];
+                int tmp = virtual_memory.get_left_unused();
+                virtual_memory.set_left_unused(--tmp);
+                cout<<"referencing page "<<page_reference[i]<<" ";
+                print_frame(virtual_memory.frame);
+                cout<<" PAGE FAULT (no victim page)"<<endl;
+                count_fault++;
+            }
+            else
+            {
+                map<int,int> backward_count;
+                int least_recently_page;
+                int k = i-1;
+                while(backward_count.size() < F && k >= 0)
+                {
+                    backward_count[page_reference[k]]++;
+                    least_recently_page = page_reference[k];
+                    k--;
+                }
+                int least_recently_page_index;
+                for(int n = 0 ; n < F ; n++)
+                {
+                    if(least_recently_page == virtual_memory.frame[n])
+                        least_recently_page_index = n;
+                }
+                virtual_memory.frame[least_recently_page_index] = page_reference[i];
+                cout<<"referencing page "<<page_reference[i]<<" ";
+                print_frame(virtual_memory.frame);
+                cout<<" PAGE FAULT (victim page "<<least_recently_page<<")"<<endl;
+                count_fault++;
+            }
+        }
+        //This page has existed in the frame.
+        else
+        {
+            cout<<"referencing page "<<page_reference[i]<<" ";
+            print_frame(virtual_memory.frame);
+            cout<<endl;
+        }
+    }
+    cout<<"End of LRU simulation ("<<count_fault<<" page faults)"<<endl;
+    cout<<endl;
+}
+//------------------------------------------------LFU replacement algo----------------------------
+void LFU(vector<int> page_reference, Virtual_memory virtual_memory)
+{
+    int count_fault = 0;
+    cout<<"Simulating LFU with fixed frame size of 3"<<endl;
+    map<int, int> page_times;
+    for(int i = 0; i < page_reference.size();i++)
+    {
+        bool is_find = false;
+        for(int l = 0 ; l < F;l++)
+        {
+            if(page_reference[i] == virtual_memory.frame[l])
+            {
+                is_find = true;
+                break;
+            }
+        }
+        if(!is_find)
+        {
+            //Frame is not full of pages
+            if(virtual_memory.get_left_unused() > 0)
+            {
+                int j = 0;
+                while(virtual_memory.frame[j] > 0)
+                {
+                    j++;
+                }
+                virtual_memory.frame[j] = page_reference[i];
+                int tmp = virtual_memory.get_left_unused();
+                virtual_memory.set_left_unused(--tmp);
+                cout<<"referencing page "<<page_reference[i]<<" ";
+                print_frame(virtual_memory.frame);
+                cout<<" PAGE FAULT (no victim page)"<<endl;
+                count_fault++;
+                page_times[page_reference[i]] = 1;
+            }
+            else
+            {
+                int replace_page = INT_MAX;
+                int replace_page_index = INT_MAX;
+                int replace_page_times = INT_MAX;
+                for(int q = 0 ; q < F; q++)
+                {
+                    if(page_times[virtual_memory.frame[q]] < replace_page_times )
+                    {
+                        replace_page = virtual_memory.frame[q];
+                        replace_page_times = page_times[replace_page];
+                        replace_page_index = q;
+                    }
+                    else if(page_times[virtual_memory.frame[q]] == replace_page_times 
+                            && virtual_memory.frame[q] < replace_page)
+                    {
+                        replace_page = virtual_memory.frame[q];
+                        replace_page_times = page_times[replace_page];
+                        replace_page_index = q;
+                    }
+                }
+                virtual_memory.frame[replace_page_index] = page_reference[i];
+                cout<<"referencing page "<<page_reference[i]<<" ";
+                print_frame(virtual_memory.frame);
+                cout<<" PAGE FAULT (victim page "<<replace_page<<")"<<endl;
+                count_fault++;
+                page_times[page_reference[i]] = 1;
+            }
+        }
+        //This page has existed in the frame.
+        else
+        {
+            cout<<"referencing page "<<page_reference[i]<<" ";
+            page_times[page_reference[i]]++;
+            print_frame(virtual_memory.frame);
+            cout<<endl;
+        }
+    }
+    cout<<"End of LFU simulation ("<<count_fault<<" page faults)"<<endl;
+    cout<<endl;
+}
+//================================================Virtual Memory==================================
 
 
 int main(int argc, const char * argv[]) {
@@ -204,5 +489,15 @@ int main(int argc, const char * argv[]) {
     start_simulate(event_map, memory, "Next-Fit");
     start_simulate(event_map, memory, "Best-Fit");
     start_simulate(event_map, memory, "Non-contiguous");
+
+    //================================================Virtual Memory==================================
+    ifstream in_str_2(argv[2]);
+    map<int, list<int> > page_index;
+    vector<int> page_reference;
+    read_file_2(in_str_2,page_reference,page_index);
+    Virtual_memory virtual_memory;
+    OPT(page_reference,page_index,virtual_memory);
+    LRU(page_reference,virtual_memory);
+    LFU(page_reference,virtual_memory);
     return 0;
 }
